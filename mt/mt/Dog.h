056df7e04a1e6da502f80a0e0dedee1fb1a27d31
zhoucan <1160908321@qq.com>
2016-08-23 11:22:15 +0800

    创建Dog类

---
+++ mt/mt/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  mt
+//
+//  Created by zhoucan on 2016/8/23.
+//  Copyright © 2016年 verfing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ mt/mt/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  mt
+//
+//  Created by zhoucan on 2016/8/23.
+//  Copyright © 2016年 verfing. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
